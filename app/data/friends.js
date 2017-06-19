f9ffbcb50756f2e1c37dad197e726c2c6d7e52e9
Joseph Hernandez <josephhernandez@Josephs-MacBook-Pro.local>
2017-06-19 00:24:13 -0700

    Almost complete

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var defaultFriends = [
+    {
+    name: "User 1",
+    photo: "https://s-media-cache-ak0.pinimg.com/originals/32/85/f8/3285f8442738bd932ccdc2842b67b0c7.png",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1],
+  },
+  {
+    name: "User 2",
+    photo: "http://vignette2.wikia.nocookie.net/simpsons/images/0/02/Homer_Simpson_2006.png/revision/latest?cb=20091207194310",
+    scores: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1],
+  },
+  {
+    name: "User 3",
+    photo: "https://static.comicvine.com/uploads/scale_small/11/114183/5147869-abraham_simpson.png",
+    scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  }
+];
+
+module.exports = defaultFriends;
